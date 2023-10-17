605efa0320f46ae4626f1cc460646725c14f0ed0
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-10-17 17:00:28 -0600

    Create VerifiedContactPhoneRequiredMixin and use it for CreateSupportGroupView

--- agir/authentication/view_mixins.py
+++ agir/authentication/view_mixins.py
@@ -1,14 +1,17 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import (
     AccessMixin,
     LoginRequiredMixin,
 )
 from django.contrib.auth.views import redirect_to_login
 from django.core.exceptions import PermissionDenied
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.template.response import TemplateResponse
+from django.utils.translation import gettext_lazy as _
 from rules.contrib.views import PermissionRequiredMixin
 
 from agir.authentication.utils import is_hard_logged
+from agir.lib.utils import front_url_lazy
 
 SoftLoginRequiredMixin = LoginRequiredMixin
 
@@ -32,6 +35,29 @@ class HardLoginRequiredMixin(AccessMixin):
                 self.get_login_url(),
                 self.get_redirect_field_name(),
             )
+        return super().dispatch(request, *args, **kwargs)
+
+
+class VerifiedContactPhoneRequiredMixin(AccessMixin):
+    unverified_phone_redirect_target = front_url_lazy("send_validation_sms")
+    unverified_phone_message = _("Vous devez ajouter et verifier un numéro de téléphone avant de pouvoir continuer")
+
+    def dispatch(self, request, *args, **kwargs):
+        if not is_hard_logged(request):
+            # TOUJOURS rediriger vers le login pour du hardlogin
+            # N.B. on ne peut pas redéfinir handle_no_permission, sinon on rend impossible l'utilisation
+            # de ce mixin en même temps qu'un des mixins de permissions.
+            return redirect_to_login(
+                self.request.get_full_path(),
+                self.get_login_url(),
+                self.get_redirect_field_name(),
+            )
+        person = request.user.person
+
+        if not person.contact_phone or not person.contact_phone_status == person.CONTACT_PHONE_VERIFIED:
+            messages.add_message(request, messages.WARNING, self.unverified_phone_message)
+            return HttpResponseRedirect(self.unverified_phone_redirect_target)
+
         return super().dispatch(request, *args, **kwargs)
 
 
