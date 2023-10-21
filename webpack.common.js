23060282f86caddd9a4a8da9fc7033c25c79e968
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-10-20 19:40:56 -0600

    fix: webpack build

--- webpack.common.js
+++ webpack.common.js
@@ -128,12 +128,13 @@ let _cachedOtherEntryFiles;
 const getOtherEntryFiles = (compilation) => {
   _cachedOtherEntryFiles =
     _cachedOtherEntryFiles ||
-    OTHER_ENTRIES.map((name) =>
-      compilation.namedChunkGroups
-        .get(name)
-        .chunks.map((c) => Array.from(c.files))
-        .flat(),
-    )
+    OTHER_ENTRIES.filter((name) => compilation.namedChunkGroups.get(name))
+      .map((name) =>
+        compilation.namedChunkGroups
+          .get(name)
+          .chunks.map((c) => Array.from(c.files))
+          .flat(),
+      )
       .flat()
       .filter((file) => !getAppEntryFiles(compilation).includes(file));
 
